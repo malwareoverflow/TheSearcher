587c043c35064672dca092c33b3e505648154c82
Taha Temuri <tahatemuri7@outlook.com>
2018-03-02 11:17:30 +0500

    code commint

---
+++ DirectoryIterator/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace DirectoryIterator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Path = new System.Windows.Forms.TextBox();
+            this.Searchvalue = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Deletechoice = new System.Windows.Forms.CheckBox();
+            this.Extensionvalue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ExtensionList = new System.Windows.Forms.DataGridView();
+            this.Timer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ExtensionList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Path
+            // 
+            this.Path.Location = new System.Drawing.Point(89, 86);
+            this.Path.Name = "Path";
+            this.Path.Size = new System.Drawing.Size(100, 20);
+            this.Path.TabIndex = 0;
+            // 
+            // Searchvalue
+            // 
+            this.Searchvalue.Location = new System.Drawing.Point(89, 112);
+            this.Searchvalue.Name = "Searchvalue";
+            this.Searchvalue.Size = new System.Drawing.Size(100, 20);
+            this.Searchvalue.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(89, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Deletechoice
+            // 
+            this.Deletechoice.AutoSize = true;
+            this.Deletechoice.Location = new System.Drawing.Point(89, 209);
+            this.Deletechoice.Name = "Deletechoice";
+            this.Deletechoice.Size = new System.Drawing.Size(57, 17);
+            this.Deletechoice.TabIndex = 5;
+            this.Deletechoice.Text = "Delete";
+            this.Deletechoice.UseVisualStyleBackColor = true;
+            // 
+            // Extensionvalue
+            // 
+            this.Extensionvalue.Location = new System.Drawing.Point(89, 138);
+            this.Extensionvalue.Name = "Extensionvalue";
+            this.Extensionvalue.Size = new System.Drawing.Size(100, 20);
+            this.Extensionvalue.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Extension";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Filename";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Directory";
+            // 
+            // ExtensionList
+            // 
+            this.ExtensionList.BackgroundColor = System.Drawing.Color.White;
+            this.ExtensionList.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ExtensionList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ExtensionList.GridColor = System.Drawing.SystemColors.ControlLight;
+            this.ExtensionList.Location = new System.Drawing.Point(303, 67);
+            this.ExtensionList.Name = "ExtensionList";
+            this.ExtensionList.RowHeadersWidth = 10;
+            this.ExtensionList.Size = new System.Drawing.Size(385, 170);
+            this.ExtensionList.TabIndex = 6;
+            // 
+            // Timer
+            // 
+            this.Timer.AutoSize = true;
+            this.Timer.Location = new System.Drawing.Point(456, 22);
+            this.Timer.Name = "Timer";
+            this.Timer.Size = new System.Drawing.Size(35, 13);
+            this.Timer.TabIndex = 11;
+            this.Timer.Text = "label4";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timertick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 261);
+            this.Controls.Add(this.Timer);
+            this.Controls.Add(this.ExtensionList);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Extensionvalue);
+            this.Controls.Add(this.Deletechoice);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Searchvalue);
+            this.Controls.Add(this.Path);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ExtensionList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Path;
+        private System.Windows.Forms.TextBox Searchvalue;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox Deletechoice;
+        private System.Windows.Forms.TextBox Extensionvalue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView ExtensionList;
+        private System.Windows.Forms.Label Timer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DirectoryIterator/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectoryIterator
+{
+    public partial class Form1 : Form
+
+    {
+        // This delegate enables asynchronous calls for setting  
+        // the text property on a TextBox control.  
+        delegate string StringArgReturningVoidDelegate();
+        private delegate void SetDataSourceDelegate(object value);
+
+      //  delegate void  StringArgReturningVoidDelegate(string text);
+
+        List<string> ChoiceExtension = new List<string>();
+
+        List<string> File = new List<string>();
+        List<string> Directories = new List<string>();
+        public Thread Timerthread;
+        public Thread Startthread;
+
+        int TotalDirectories = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                //
+                // The user selected a folder and pressed the OK button.
+                // We print the number of files found.
+                //
+               string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+              Path.Text   = folderBrowserDialog1.SelectedPath;
+
+            }
+        }
+        public void AddDir(string[] Dir)
+        {
+            try
+            {
+                foreach (string item in Dir)
+                {
+                    TotalDirectories += 1;
+
+                    Directories.Add(item);
+                }
+            }
+            catch (Exception)
+            {
+
+                
+            }
+           
+        }
+        public void AddFiles(string[] Files)
+        {
+            try
+            {
+                foreach (string item in Files)
+                {
+                    File.Add(item);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+        }
+       
+        public bool IsDirectoryEmpty(string path)
+        {
+            try
+            {
+                return !Directory.EnumerateFileSystemEntries(path).Any();
+
+            }
+            catch (Exception)
+            {
+
+                return true;
+            }
+        }
+        public void Recursive(List<string> Dirs)
+        {
+            try
+            {
+                for (var x = 0; x < Dirs.Count; x++)
+                {
+                    if (IsDirectoryEmpty(Directories[x]) == false)
+                    {
+
+                        AddDir(Directory.GetDirectories(Directories[x]));
+                        AddFiles(Directory.GetFiles(Directories[x]));
+
+
+                    }
+                    Directories.Remove(Directories[x]);
+                }
+                if (Directories.Count > 0)
+                {
+                    Recursive(Directories);
+                }
+            }
+            catch (Exception)
+            {
+
+                
+            }
+           
+        }
+        public void ShowAllFiles()
+        {
+            try
+            {
+               // MessageBox.Show($"Total files were {File.Count} and Total Directories were  {TotalDirectories}");
+                foreach (var item in File)
+                {
+                    if (System.IO.Path.GetFileName(item) == Searchvalue.Text)
+                    {
+                        MessageBox.Show($"File exist inside {item}");
+                        if (Deletechoice.Checked)
+                        {
+                            DialogResult result = MessageBox.Show($"Do you want to detele {item}", "Confirmation", MessageBoxButtons.YesNoCancel);
+                            if (result == DialogResult.Yes)
+                            {
+                               System.IO.File.Delete(item);
+
+                                MessageBox.Show($"{item} Deleted");
+
+                            }
+                           
+                           
+
+                        }
+                    }
+                    if (Extensionvalue.Text!= "")
+                    {
+                        string ext = "."+GetExtensionvalue();
+                        string filename = System.IO.Path.GetFileName(item);
+                        string pathext = System.IO.Path.GetExtension(filename);
+                        if(pathext == ext)
+                        {
+                            ChoiceExtension.Add(item);
+                        }
+                    }
+                    
+                }
+            }
+            catch (Exception)
+            {
+
+                
+            }
+           
+        }
+        public void Start()
+        {
+            try
+            {
+                if (IsDirectoryEmpty(Path.Text) == false)
+                {
+
+                    AddDir(Directory.GetDirectories(Path.Text));
+                    AddFiles(Directory.GetFiles(Path.Text));
+                   
+                    Recursive(Directories);
+                   
+                    ShowAllFiles();
+                    InserttoGrid(ChoiceExtension);
+                    MessageBox.Show(Startthread.IsAlive.ToString());
+
+                    //                    KillTheThread();
+
+                    MessageBox.Show("Done");
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.ToString());
+
+            }
+
+        }
+       
+        private string GetExtensionvalue()
+        {
+            string text="null";
+            try
+            {
+                if (this.Extensionvalue.InvokeRequired)
+                {
+                    StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(GetExtensionvalue);
+                    Extensionvalue.Invoke(new MethodInvoker(delegate { text = Extensionvalue.Text; }));
+                    return text;
+
+                }
+                else
+                {
+                    return text;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+
+                return this.Extensionvalue.Text;
+
+
+            }
+            // InvokeRequired required compares the thread ID of the  
+            // calling thread to the thread ID of the creating thread.  
+            // If these threads are different, it returns true.  
+
+        }
+        //private void SetLabel(string text)
+        //{
+        //    // InvokeRequired required compares the thread ID of the  
+        //    // calling thread to the thread ID of the creating thread.  
+        //    // If these threads are different, it returns true.  
+        //    if (this.Timer.InvokeRequired)
+        //    {
+        //        StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(SetLabel);
+        //        this.Invoke(d, new object[] { text });
+        //    }
+        //    else
+        //    {
+        //        this.Timer.Text = text;
+        //    }
+        //}
+
+        private void SetDataSource(object value)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Path of Extension");
+            foreach (var item in value as List<string>)
+            {
+
+                dt.Rows.Add(new object[] { item });
+
+            }
+            ExtensionList.DataSource = dt;
+            DataGridViewColumn secondcolumn = ExtensionList.Columns[0];
+            secondcolumn.Width = 385;
+        }
+        public void  InserttoGrid(List<string> List)
+        {
+            if (ChoiceExtension.Count >0)
+            {
+
+                if (this.ExtensionList.InvokeRequired)
+                {
+                   ExtensionList.Invoke(new SetDataSourceDelegate(SetDataSource), new Object[] { List });
+
+
+                }
+                else
+                {
+                    SetDataSource(List);
+
+                }
+
+            }
+        }
+
+        private void Timertick(object sender, EventArgs e)
+        {
+            //SetLabel(DateTime.Now.ToString("HH:mm:ss tt"));
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            MessageBox.Show("Start");
+            timer1.Enabled = true;
+            Startthread = new Thread(new ThreadStart(Start));
+            Startthread.Start();
+
+
+        }
+
+       
+    }
+}
